9ac125972717ed2373f940109ddcc77a6513bf8d
John Sizemore <jsize8@vt.edu>
2017-04-10 22:13:03 -0400

    2.4-end

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import apiRouter from './api';
 
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello Express\n');
+  res.render('index', {
+    content: 'Hello Express and EJS!'
+  });
 });
 
 server.use('/api', apiRouter)
