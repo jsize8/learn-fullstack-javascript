ce53a921c049c88685ac9df37cdf80507be3fa19
John Sizemore <jsize8@vt.edu>
2017-04-10 21:35:18 -0400

    2.3-end

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import http from 'http';
+import config from './config';
+import express from 'express';
+import apiRouter from './api';
 
-const server = http.createServer();
+const server = express();
 
-server.listen(8080);
+server.get('/', (req, res) => {
+  res.send('Hello Express\n');
+});
 
-server.on('request', (req, res) => {
-  res.write('Hello HTTP!\n');
-  setTimeout(() => {
-    res.write('I can stream!\n');
-    res.end();
-  }, 3000);
+server.use('/api', apiRouter)
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
 });
