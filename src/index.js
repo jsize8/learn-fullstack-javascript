3da9972d840a4396b8d8589f4eb18da4e39750da
John Sizemore <jsize8@vt.edu>
2017-04-12 16:56:14 -0400

    4.1-end

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 
+import data from './testData';
+console.log(data);
+
 import App from './components/App'
 
 reactDOM.render(
-  <App />,
+  <App contests={data.contests} />,
   document.getElementById('root')
 );
