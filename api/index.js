59fb75b3c213be4df54f004f02803548c4b68d96
John Sizemore <jsize8@vt.edu>
2017-04-18 10:56:14 -0400

    6.4-end

--- api/index.js
+++ api/index.js
@@ -13,4 +13,11 @@ router.get('/contests', (req, res) => {
    });
 });
 
+router.get('/contests/:contestId', (req, res) => {
+  let contest = contests[req.params.contestId];
+  contest.description = 'Lorem ipsum dolor sit amet, consectetur adip...';
+
+  res.send(contest);
+});
+
 export default router;
